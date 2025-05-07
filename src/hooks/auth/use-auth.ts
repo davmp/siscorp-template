287d70b0aeba710b48f3302db3408dadc5e3fede
davmp <davcartezini@gmail.com>
2025-05-07 14:33:16 -0300

    feat: remove auth features

--- src/hooks/auth/use-auth.ts
+++
@@ -1,6 +0,0 @@
-import { AuthProvider, AuthContext } from "@/providers/auth/auth-provider";
-import { useContext } from "react";
-
-export function useAuth() {
-  return useContext<AuthProvider>(AuthContext);
-}
